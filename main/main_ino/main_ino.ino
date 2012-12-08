e5495256d421250f3b21ea32c40d3a2884a038d3
jantore <jan@boee.org>
2012-12-08 14:14:02 +0100

    first release

--- main/main_ino/main_ino.ino
+++ main/main_ino/main_ino.ino
@@ -1,23 +1,33 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
 
 #define ONE_WIRE_BUS 2
+
+
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+Adafruit_8x8matrix matrix1 = Adafruit_8x8matrix();
+Adafruit_8x8matrix matrix2 = Adafruit_8x8matrix();
 
 OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
 DeviceAddress temp0;
-DeviceAddress temp1;
+
+boolean toggle =0;
 
 void setup(void){
   Serial.begin(9600);
-
   sensors.begin();
+  matrix.begin(0x72);
+  matrix1.begin(0x71);
+  matrix2.begin(0x70);
   if (!sensors.getAddress(temp0, 0)) Serial.println("Unable to find address for temp0"); 
-  if (!sensors.getAddress(temp1, 1)) Serial.println("Unable to find address for temp1"); 
   sensors.setResolution(temp0, 12);
-  sensors.setResolution(temp1, 12);
+  pinMode(7,OUTPUT);
 }
 
 void printTemperature(DeviceAddress deviceAddress){
@@ -26,12 +36,46 @@ void printTemperature(DeviceAddress deviceAddress){
   Serial.println(tempC);
 }
 
-void loop(void){ 
+void print(float p){
+  
+  matrix1.clear();
+  matrix1.setTextSize(0.5);
+  matrix1.setTextWrap(false);
+  matrix1.setTextColor(LED_ON);
+  matrix1.setCursor(0,0);
+  matrix1.print(p);
+  matrix1.writeDisplay();
+
+  matrix.clear();
+  matrix.setTextSize(0.5);
+  matrix.setTextWrap(false);
+  matrix.setTextColor(LED_ON);
+  matrix.setCursor(-8,0);
+  matrix.print(p);
+  matrix.writeDisplay();
+
+  matrix2.clear();
+  matrix2.setTextSize(0.5);
+  matrix2.setTextWrap(false);
+  matrix2.setTextColor(LED_ON);
+  matrix2.setCursor(-16,0);
+  matrix2.print(p);
+  matrix2.writeDisplay();
+}
+
+void loop(void){
   sensors.requestTemperatures();
   printTemperature(temp0);
-  printTemperature(temp1);
   int value = analogRead(A0);
   float c = (value/50.0F) + 50;
   Serial.println(c);
+  float r = sensors.getTempC(temp0);
+  print(toggle ? r:c);
+  toggle = !toggle;
+  if (r < c) {
+    digitalWrite(7, HIGH);
+  }else{
+    digitalWrite(7, LOW);
+  }
 }
 
